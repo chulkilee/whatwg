259f119e8285dbc1440086654165009db222b3e7
Chulki Lee <chulki.lee@gmail.com>
2021-03-07 08:59:29 +0900

    chore: remove boilerplate

--- lib/whatwg.ex
+++ lib/whatwg.ex
@@ -2,17 +2,4 @@ defmodule WHATWG do
   @moduledoc """
   Documentation for `WHATWG`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> WHATWG.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/whatwg_test.exs
+++ test/whatwg_test.exs
@@ -1,8 +1,4 @@
 defmodule WHATWGTest do
   use ExUnit.Case
   doctest WHATWG
-
-  test "greets the world" do
-    assert WHATWG.hello() == :world
-  end
 end
