4b5bea8b29ba47176b8a835f6ecfde6d3f2fe5a0
Chulki Lee <chulki.lee@gmail.com>
2021-03-07 15:15:00 +0900

    build: allow elixir 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WHATWG.MixProject do
     [
       app: :whatwg,
       version: "0.0.1-dev",
-      elixir: "~> 1.11",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package()
