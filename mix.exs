bcae57fcfec3d4737b90b96955d8eb9d10c9e216
Chulki Lee <chulki.lee@gmail.com>
2021-03-07 14:52:02 +0900

    chore: 0.0.1-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WHATWG.MixProject do
   def project do
     [
       app: :whatwg,
-      version: "0.1.0",
+      version: "0.0.1-dev",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
